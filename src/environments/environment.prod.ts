9ba457dc4f5e73d6579b1db9a7b3086a4413404a
Michael Ashefor <thatsureboy@gmail.com>
2019-06-28 15:08:19 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCekStLt5jce5dy7KY3dPsjkrIZG0CdQ60",
+    authDomain: "michaelashefor.firebaseapp.com",
+    databaseURL: "https://michaelashefor.firebaseio.com",
+    projectId: "michaelashefor",
+    storageBucket: "",
+    messagingSenderId: "329522172279",
+    appId: "1:329522172279:web:d6f58d48f7db53a2"
+  }
 };
